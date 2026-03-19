31d38c65ce38b1719f471fc967a6d858a548efec
Gabriel <50024453+TheGabrielVieira@users.noreply.github.com>
2026-03-19 01:31:24 -0300

    first commit

---
+++ src/main/java/br/com/projeto_hackathon_api_vacinacao_paciente/ProjetoHackathonApiVacinacaoPacienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto_hackathon_api_vacinacao_paciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoHackathonApiVacinacaoPacienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoHackathonApiVacinacaoPacienteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/projeto_hackathon_api_vacinacao_paciente/ProjetoHackathonApiVacinacaoPacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto_hackathon_api_vacinacao_paciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoHackathonApiVacinacaoPacienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
